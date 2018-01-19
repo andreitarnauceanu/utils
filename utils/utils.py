11a105a410f7d745a5ddb8b7f4bca7e91aac75df
Cloud User <ec2-user@ip-10-0-1-77.eu-central-1.compute.internal>
2018-01-19 13:01:04 +0000

    Pre-signed and shorturl added

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,22 @@ import os
 import boto3
 from PIL import Image
 from boto3.s3.transfer import S3Transfer
+import json
+import requests
+
+GOOGLE_URL_SHORTEN_API = 'AIzaSyBrnkM64WKqmKa_FmRbR6WIZeAu-hXE-6I' # IP Address restrictions
+
+def shorten(url):
+   req_url = 'https://www.googleapis.com/urlshortener/v1/url?key=' + GOOGLE_URL_SHORTEN_API
+   payload = {'longUrl': url}
+   headers = {'content-type': 'application/json'}
+   r = requests.post(req_url, data=json.dumps(payload), headers=headers)
+   resp = json.loads(r.text)
+   return resp['id']
+
+def pre_signed_url(key, bucket):
+        s3Client = boto3.client('s3')
+        return s3Client.generate_presigned_url('get_object', Params = {'Bucket': bucket, 'Key': key}, ExpiresIn = 300)
 
 def crop(image_path, coords, saved_location):
   """
